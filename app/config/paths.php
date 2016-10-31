d812cff16f3e66808931a6f4a184308df206a687
Taffarel Vitor <taffareloliveira94@gmail.com>
2016-10-31 04:42:05 -0200

    Adicionar o arquivo paths.php

---
+++ app/config/paths.php
@@ -0,0 +1,7 @@
+<?php
+
+//url = apenas oNomeQueVoceColocou
+define('URL','http://'.$_SERVER['SERVER_NAME'].'/ERPwNFe/');
+
+//folder = oNomeQueVoceColocou/app
+define('Folder','http://'.$_SERVER['SERVER_NAME'].'/ERPwNFe/app/public/');
